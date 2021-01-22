c12d438742d8c0de74662ce7e7305f412cddefcb
weronikadomczewska <w.domczewska@gmail.com>
2021-01-22 14:57:25 +0100

    src/monopoly_table.py: zmieniono program do statystyk na funkcję statisics(data)

--- src/monopoly_table.py
+++ src/monopoly_table.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import random
 import subprocess
+import os
 
 #TODO: dopisz do readme konieczność zainstalowania numpy
 #TODO: ustaw kolory wierszy tak, aby zgadzały się z kolorami pionków graczy
@@ -9,87 +10,87 @@ import subprocess
 #w przypadku bankruta wszystkie statystyki sa zerowane
 
 #dane
-
-data = [
-
-    ['255', '18', '10'],
-    ['10', '0', '15'],
-    ['0', '0', '0'],
-    ['100', '15', '23']
-
-]
-
-#rangi
-
-#szukam najwięcej ECTS
-max_ects = max(data,key=lambda x:x[0])
-if len(max_ects) == 3:
-    max_ects.append("geniusz!")
-else:
-    max_ects[3] += ", geniusz!"
-
-#szukam najwięcej przedmiotów
-max_p = max(data,key=lambda x:x[1])
-if len(max_p) == 3:
-    max_p.append("pracoholik")
-else:
-    max_p[3] += ", pracoholik"
-
-#szukam najwięcej napraw
-max_np= max(data,key=lambda x:x[2])
-if len(max_np) == 3:
-    max_np.append("(nie)złota rączka!")
-else:
-    max_np[3] += ", (nie)złota rączka!"
-
-for p in data:
-    if len(p) < 4:
-        p.append("")
-
-print(data)
-
-#zamieniam zera na "bankrut!"
-for i in range(len(data)):
-    cnt=0
-    for j in range(len(data[i])):
-        if data[i][j] == '0':
-            cnt += 1
-    if cnt == 3:
-        data[i] = ['bankrut', 'bankrut', 'bankrut', 'bankrut']
-
-
-#wykres
-fig, ax = plt.subplots(figsize=(12, 5)) 
-ax.set_axis_off()
-
-columns = ("ECTS'y", "zaliczone przedmioty", "naprawy komputera", "ranga")
-rows = ["Gracz " + str(i+1) for i in range(len(data))]
-print(rows)
-
-#kolory graczy
-player_colors = ["yellow", "green", "lightblue", "red"]
-
-table = ax.table( 
-    cellText=data, 
-    rowLabels=rows,  
-    colLabels=columns, 
-    rowColours=player_colors,  
-    colColours=["lightblue"]* len(columns), 
-    cellLoc='center',
-    colWidths=[0.25, 0.25, 0.25, 0.25], 
-    loc='upper left',
+def statistics(data):
     
-)
-    
-ax.set_title('Koniec gry!', fontsize=30)
-table.scale(1, 4)
-table.auto_set_font_size(False)
-table.set_fontsize(15)
-plt.tight_layout()
+    #rangi
+    #szukam najwięcej ECTS
+    max_ects = max(data,key=lambda x:x[0])
+    if len(max_ects) == 3:
+        max_ects.append("geniusz!")
+    else:
+        max_ects[3] += ", geniusz!"
 
-#zapis do pliku - odkomentuj!
-# subprocess.run(["touch", "plot.png"])
-# plt.savefig(fname="plot.png")
-   
-plt.show() 
+    #szukam najwięcej przedmiotów
+    max_p = max(data,key=lambda x:x[1])
+    if len(max_p) == 3:
+        max_p.append("pracoholik")
+    else:
+        max_p[3] += ", pracoholik"
+
+    #szukam najwięcej napraw
+    max_np= max(data,key=lambda x:x[2])
+    if len(max_np) == 3:
+        max_np.append("(nie)złota rączka!")
+    else:
+        max_np[3] += ", (nie)złota rączka!"
+
+    for p in data:
+        if len(p) < 4:
+            p.append("")
+
+    #zamieniam zera na "bankrut!"
+    for i in range(len(data)):
+        cnt=0
+        for j in range(len(data[i])):
+            if data[i][j] == '0':
+                cnt += 1
+        if cnt == 3:
+            data[i] = ['bankrut', 'bankrut', 'bankrut', 'bankrut']
+
+
+    #wykres
+    fig, ax = plt.subplots(figsize=(12, 5)) 
+    ax.set_axis_off()
+
+    columns = ("ECTS'y", "zaliczone przedmioty", "naprawy komputera", "ranga")
+    rows = ["Gracz " + str(i+1) for i in range(len(data))]
+    # print(rows)
+
+    #kolory graczy
+    player_colors = ["yellow", "green", "lightblue", "red"]
+
+    table = ax.table( 
+        cellText=data, 
+        rowLabels=rows,  
+        colLabels=columns, 
+        rowColours=player_colors,  
+        colColours=["lightblue"]* len(columns), 
+        cellLoc='center',
+        colWidths=[0.25, 0.25, 0.25, 0.25], 
+        loc='upper left',
+        
+    )
+        
+    ax.set_title('Koniec gry!', fontsize=30)
+    table.scale(1, 4)
+    table.auto_set_font_size(False)
+    table.set_fontsize(15)
+    plt.tight_layout()
+
+    # zapis do pliku - odkomentuj!
+    plt.savefig(fname="res/table.png")
+    # plt.show()
+
+
+#test
+# data = [
+
+#     ['255', '18', '10'],
+#     ['10', '0', '15'],
+#     ['0', '0', '0'],
+#     ['100', '15', '23']
+
+# ]
+
+# statistics(data)
 
